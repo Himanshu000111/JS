56f55bc96b887f9bc8cc20db8202bb0cc49700f7
Himanshu000111 <145653053+Himanshu000111@users.noreply.github.com>
2023-09-21 10:25:18 +0000

    Variable in Javascript

---
+++ 01VARIABLE.js
@@ -0,0 +1,12 @@
+const fruitPrice = 100
+let fruitName = "Apple"
+var fruitQUALITY = "GOOD"
+FruitCity = "Jaipur"
+console.log(FruitCity)
+
+console.table([fruitPrice, fruitName, fruitQUALITY, FruitCity])
+
+FruitCity = "Bengaluru"
+console.log(FruitCity)
+
+/* prefer not to use var because of issue in b;ock scope and functional scope */
