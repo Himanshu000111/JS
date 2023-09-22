d78c7e53baf583dadfa7837957f6dae3bda3d34f
Himanshu000111 <145653053+Himanshu000111@users.noreply.github.com>
2023-09-22 10:44:41 +0000

    data types for js

---
+++ 02datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer versions
+
+// alert (3+3)  it will not work because we are using node js not browser and it works only in browser.
+
+console.log(3
+    +3); // this willl work but it does not look pretty so code should also be readable.
+
+    // data types
+    // number => 2 to power 53
+    // string => ""
+    //bigint => it is used when we have to take a big value 
+    //boolean => true/false
+    //null => standalone (when we want to return no value in case of error)
+    //undefined => when only variable is defined but no value is assigned to that variable.
+    // symbol => when to define something unique 
+
+    // object
+
+    console.log(typeof "Himanshu"); //it is used when we want to know the data type of a value or a variable
+    console.log(typeof 3)
+    console.log(typeof undefined); // it will return undefined only 
+
+    console.log(typeof null); // it will return object as data type 
